61517c4028338ec797b5a5844ef9d3f946c3ca86
99gu5843 <99gu5843@NTX52DESA354.seg-social.ss>
2022-03-08 09:06:31 +0100

    Incluir edgeWebDriver. Dos tests, uno genera error

--- src/test/java/junit/XV23VC0101Test.java
+++ src/test/java/junit/XV23VC0101Test.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Dimension;
@@ -20,7 +21,15 @@ public class XV23VC0101Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
+	//Cargar el webdriver definido como parametro de entrada en la ejecucion
+	  
+	if(System.getProperty("webdriver.edge.driver")!=null) {
+		driver = new EdgeDriver();
+	}else if(System.getProperty("webdriver.chrome.driver")!=null) {
+		driver = new ChromeDriver();
+	}else {
+		System.out.println("No se ha definido un webdriver");
+	}
     js = (JavascriptExecutor) driver;
   }
   @After
@@ -38,11 +47,19 @@ public class XV23VC0101Test {
 	    WebDriverWait wait = new WebDriverWait(driver, duration);
 	    wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
 	    driver.findElement(By.id("configuracionIntranetDSServicio")).click();
-	    driver.findElement(By.cssSelector("tr:nth-child(1)")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
 	    driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
 	    driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
 	    
-	    driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	    try {
+	    	driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	    	
+	    }catch (Exception e) {
+	    	driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
+		    wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
+	    	driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+		}
+	    
 	    driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
 	    driver.findElement(By.id("submitIntranetDSServicio")).click();
 	    driver.findElement(By.id("sum1")).sendKeys("1");
@@ -56,7 +73,7 @@ public class XV23VC0101Test {
 	      String value = driver.findElement(By.id("res1")).getAttribute("value");
 	      assertEquals(value, "13");
 	    }
-  }
+}
   
   @Test
   public void XV23VC0101_2_JUNIT() {
@@ -69,11 +86,19 @@ public class XV23VC0101Test {
 	    WebDriverWait wait = new WebDriverWait(driver, duration);
 	    wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
 	    driver.findElement(By.id("configuracionIntranetDSServicio")).click();
-	    driver.findElement(By.cssSelector("tr:nth-child(1)")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
 	    driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
 	    driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
 	    
-	    driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	    try {
+	    	driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	    	
+	    }catch (Exception e) {
+	    	driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
+		    wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
+	    	driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+		}
+	    
 	    driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
 	    driver.findElement(By.id("submitIntranetDSServicio")).click();
 	    driver.findElement(By.id("sum1")).sendKeys("1");
@@ -85,7 +110,7 @@ public class XV23VC0101Test {
 	    driver.findElement(By.id("res1")).click();
 	    {
 	      String value = driver.findElement(By.id("res1")).getAttribute("value");
-	      assertEquals(value, "13");
+	      assertEquals(value, "2");
 	    }
   }
 }
