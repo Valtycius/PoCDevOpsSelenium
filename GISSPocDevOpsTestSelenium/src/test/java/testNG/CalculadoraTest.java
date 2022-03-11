6194bee8a6c1d9c4cedeae71fdd2d633a1bf1b3c
99gu5843 <99gu5843@NTX52DESA178.seg-social.ss>
2022-03-11 13:44:04 +0100

    Controlar parámetro webdriver. Marcar como fail los tests si se produce una excepción

--- GISSPocDevOpsTestSelenium/src/test/java/testNG/CalculadoraTest.java
+++ GISSPocDevOpsTestSelenium/src/test/java/testNG/CalculadoraTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
+import java.io.File;
 import java.time.Duration;
 
 public class CalculadoraTest {
@@ -23,99 +24,130 @@ public class CalculadoraTest {
 	@BeforeTest
 	public void setUp() {
 		// Cargar el webdriver definido como parametro de entrada en la ejecucion
-
-		if (System.getProperty("webdriver.edge.driver") != null) {
-			driver = new EdgeDriver();
-		} else if (System.getProperty("webdriver.chrome.driver") != null) {
-			driver = new ChromeDriver();
-		} else {
-			System.out.println("No se ha definido un webdriver");
+		try {
+			if (System.getProperty("webdriver.edge.driver") != null && System.getProperty("webdriver.edge.driver").length()>0) {
+				
+				File tempFile = new File(System.getProperty("webdriver.edge.driver"));
+				if(tempFile.exists()) {			
+					driver = new EdgeDriver();
+				}else {
+					System.out.println();
+					System.out.println("ERROR: El driver indicado no existe (" + System.getProperty("webdriver.edge.driver") + ")");
+				}
+			}else if (System.getProperty("webdriver.chrome.driver") != null && System.getProperty("webdriver.chrome.driver").length()>0) {
+				
+				File tempFile = new File(System.getProperty("webdriver.chrome.driver"));
+				if(tempFile.exists()) {			
+					driver = new ChromeDriver();
+				}else {
+					System.out.println();
+					System.out.println("ERROR: El driver indicado no existe (" + System.getProperty("webdriver.chrome.driver") + ")");
+				}
+			} else {
+				System.out.println("ERROR: No se ha definido un webdriver");
+			}
+			js = (JavascriptExecutor) driver;
+		}catch (Exception e) {
+			System.out.println("Error configurando el webdriver: " + e.getMessage());
 		}
-		js = (JavascriptExecutor) driver;
 	}
 
 	@AfterTest
 	public void tearDown() {
-		driver.quit();
+		try {
+			driver.quit();
+		}catch (Exception e) {
+			System.out.println("Error al cerrar el webdriver " + e.getMessage());
+		}
 	}
 
 	@Test
 	public void Suma1y2multi3() {
 		// driver.get("http://10.99.104.217:9081/ProsaPortal7/index.jsp");
-		driver.get(
-				"http://" + System.getProperty("host") + ":" + System.getProperty("port") + "/ProsaPortal7/index.jsp");
-		driver.manage().window().setSize(new Dimension(1038, 684));
-
-		driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
-		Duration duration = Duration.ofSeconds(10);
-		WebDriverWait wait = new WebDriverWait(driver, duration);
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
-		driver.findElement(By.id("configuracionIntranetDSServicio")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
-		driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
-		driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
-
 		try {
-			driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
-
-		} catch (Exception e) {
+			driver.get(
+					"http://" + System.getProperty("host") + ":" + System.getProperty("port") + "/ProsaPortal7/index.jsp");
+			driver.manage().window().setSize(new Dimension(1038, 684));
+	
 			driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
-			wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
-			driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
-		}
-
-		driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
-		driver.findElement(By.id("submitIntranetDSServicio")).click();
-		driver.findElement(By.id("sum1")).sendKeys("1");
-		driver.findElement(By.id("sum2")).click();
-		driver.findElement(By.id("sum2")).sendKeys("2");
-		driver.findElement(By.id("multi")).click();
-		driver.findElement(By.id("multi")).sendKeys("3");
-		driver.findElement(By.id("SPM.ACC.CALCULAR1")).click();
-		driver.findElement(By.id("res1")).click();
-		{
-			String value = driver.findElement(By.id("res1")).getAttribute("value");
-			Assert.assertEquals(value, "10", "El resultado no es correcto ");
+			Duration duration = Duration.ofSeconds(10);
+			WebDriverWait wait = new WebDriverWait(driver, duration);
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
+			driver.findElement(By.id("configuracionIntranetDSServicio")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
+			driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
+			driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
+	
+			try {
+				driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	
+			} catch (Exception e) {
+				driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
+				driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+			}
+	
+			driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
+			driver.findElement(By.id("submitIntranetDSServicio")).click();
+			driver.findElement(By.id("sum1")).sendKeys("1");
+			driver.findElement(By.id("sum2")).click();
+			driver.findElement(By.id("sum2")).sendKeys("2");
+			driver.findElement(By.id("multi")).click();
+			driver.findElement(By.id("multi")).sendKeys("3");
+			driver.findElement(By.id("SPM.ACC.CALCULAR1")).click();
+			driver.findElement(By.id("res1")).click();
+			{
+				String value = driver.findElement(By.id("res1")).getAttribute("value");
+				Assert.assertEquals(value, "10", "El resultado no es correcto ");
+			}
+		}catch (Exception e) {
+			System.out.println("Error al generar el test " + e.getMessage());
+			Assert.assertTrue(false, "Test marcado como fail debido a una excepción durante la ejecución");
 		}
 	}
 
 	@Test
 	public void Suma1y2multi4() {
 		// driver.get("http://10.99.104.217:9081/ProsaPortal7/index.jsp");
-		driver.get(
-				"http://" + System.getProperty("host") + ":" + System.getProperty("port") + "/ProsaPortal7/index.jsp");
-		driver.manage().window().setSize(new Dimension(1038, 684));
-
-		driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
-		Duration duration = Duration.ofSeconds(10);
-		WebDriverWait wait = new WebDriverWait(driver, duration);
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
-		driver.findElement(By.id("configuracionIntranetDSServicio")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
-		driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
-		driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
-
-		try {
-			driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
-
-		} catch (Exception e) {
+		try{
+			driver.get(
+					"http://" + System.getProperty("host") + ":" + System.getProperty("port") + "/ProsaPortal7/index.jsp");
+			driver.manage().window().setSize(new Dimension(1038, 684));
+	
 			driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
-			wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
-			driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
-		}
-
-		driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
-		driver.findElement(By.id("submitIntranetDSServicio")).click();
-		driver.findElement(By.id("sum1")).sendKeys("1");
-		driver.findElement(By.id("sum2")).click();
-		driver.findElement(By.id("sum2")).sendKeys("2");
-		driver.findElement(By.id("multi")).click();
-		driver.findElement(By.id("multi")).sendKeys("4");
-		driver.findElement(By.id("SPM.ACC.CALCULAR1")).click();
-		driver.findElement(By.id("res1")).click();
-		{
-			String value = driver.findElement(By.id("res1")).getAttribute("value");
-			Assert.assertEquals(value, "13", "El resultado no es correcto ");
+			Duration duration = Duration.ofSeconds(10);
+			WebDriverWait wait = new WebDriverWait(driver, duration);
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("configuracionIntranetDSServicio")));
+			driver.findElement(By.id("configuracionIntranetDSServicio")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("codSilcon")));
+			driver.findElement(By.id("codSilcon")).sendKeys("IDUS7143");
+			driver.findElement(By.id("codSilcon")).sendKeys(Keys.ENTER);
+	
+			try {
+				driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+	
+			} catch (Exception e) {
+				driver.findElement(By.id("ui-accordion-accordion-header-1")).click();
+				wait.until(ExpectedConditions.elementToBeClickable(By.id("INTRANETDS_SERVICIO")));
+				driver.findElement(By.id("INTRANETDS_SERVICIO")).click();
+			}
+	
+			driver.findElement(By.id("INTRANETDS_SERVICIO")).sendKeys("XV23VC01");
+			driver.findElement(By.id("submitIntranetDSServicio")).click();
+			driver.findElement(By.id("sum1")).sendKeys("1");
+			driver.findElement(By.id("sum2")).click();
+			driver.findElement(By.id("sum2")).sendKeys("2");
+			driver.findElement(By.id("multi")).click();
+			driver.findElement(By.id("multi")).sendKeys("4");
+			driver.findElement(By.id("SPM.ACC.CALCULAR1")).click();
+			driver.findElement(By.id("res1")).click();
+			{
+				String value = driver.findElement(By.id("res1")).getAttribute("value");
+				Assert.assertEquals(value, "13", "El resultado no es correcto ");
+			}
+		}catch (Exception e) {
+			System.out.println("Error al generar el test " + e.getMessage());
+			Assert.assertTrue(false, "Test marcado como fail debido a una excepción durante la ejecución");
 		}
 	}
 }
